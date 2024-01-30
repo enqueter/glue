fa444089eb97dc5dfcf0a6a2d83857a202f84a6e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-30 21:44:38 +0000

    Update src/database/algorithm.py

--- src/database/algorithm.py
+++ src/database/algorithm.py
@@ -18,28 +18,31 @@ class Algorithm:
         https://github.com/awsdocs/aws-doc-sdk-examples/tree/main/python/example_code/glue#code-examples
     """
 
-    def __init__(self):
+    def __init__(self, parameters: dict):
         """
         The constructor
+
+        :param parameters: A dictionary of parameters
         """
+
+        self.__parameters = parameters
 
         # Glue Client
         self.__glue_client = boto3.client(service_name='glue')
 
-    def delete_database(self, name: str) -> bool:
+    def delete_database(self) -> bool:
         """
         https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/glue/client/delete_database.html
 
-        :param name:
         :return:
         """
 
         try:
-            self.__glue_client.delete_database(Name=name)
-            logging.log(level=logging.INFO, msg=f'Database {name} has been deleted.')
+            self.__glue_client.delete_database(Name=self.__parameters['database_name'])
+            logging.log(level=logging.INFO, msg=f'Database {self.__parameters['database_name']} has been deleted.')
             return True
         except self.__glue_client.exceptions.EntityNotFoundException:
-            logging.log(level=logging.INFO, msg=f'Database {name} does not exist.')
+            logging.log(level=logging.INFO, msg=f'Database {self.__parameters['database_name']} does not exist.')
             return True
         except self.__glue_client.exceptions.OperationTimeoutException:
             logging.log(level=logging.INFO, msg='Time out.')
