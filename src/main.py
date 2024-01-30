49c1ebb4eebf8e2fbcbd589175752f13ef8fb516
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-30 18:29:25 +0000

    Add src/main.py

---
+++ src/main.py
@@ -0,0 +1,24 @@
+
+import logging
+import os
+import sys
+
+
+def main():
+
+    logger = logging.getLogger(__name__)
+    logger.info('Glue')
+
+
+if __name__ == '__main__':
+    # Paths
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    main()
