58de25f851f33e85eec420a2321922fe6ba2ef79
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-04-24 17:17:46 +0100

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 """
 The module main.py
 """
+import logging
 import argparse
 import os
 import sys
@@ -11,9 +12,12 @@ def main():
     The entry point
     """
 
+    logger = logging.getLogger(__name__)
+
     # Setting up
     dictionary = src.functions.serial.Serial().read(uri=args.elements)
     node: dict = dictionary['parameters']
+    logger.info(node)
 
     parameters = src.elements.glue_paramaters.GlueParameters(**node)
     src.select.Select().exc(parameters=parameters)
@@ -28,6 +32,10 @@ if __name__ == '__main__':
     root = os.getcwd()
     sys.path.append(root)
     sys.path.append(os.path.join(root, 'src'))
+
+    # logging
+    logging.basicConfig(level=logging.INFO, format='%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
 
     # Modules
     import src.functions.cache
